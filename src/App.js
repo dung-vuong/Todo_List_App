d317fb2a9eb894ed03874c1f96fcab643f253bd0
Dung Vuong <dung.vuong@sjsu.edu>
2022-03-12 13:31:29 -0800

    passing parameter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, {useState} from "react";
 import './App.css';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>DungV's Todo List</h1>
       </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,25 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({setInputText, todos, setTodos, inputText}) => {
+    const inputTextHandler = (e) => {
+        console.log(e.target.value)
+        setInputText(e.target.value)
+    }
+    const submitTodoHandler = (e) => {
+        e.preventDefault()
+        setTodos([
+            ...todos, 
+            {text: inputText, completed: false, id: Math.random() * 1000}
+        ])
+    }
     return(
         <form>
-            <input type="text" class="todo-input" />
-            <button class="todo-button" type="submit">
-                <i class="fas fa-plus-square"></i>
+            <input onChange={inputTextHandler} type="text" className="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
             </button>
-            <div class="select">
-                <select name="todos" class="filter-todo">
+            <div className="select">
+                <select name="todos" className="filter-todo">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="uncompleted">Uncompleted</option>
@@ -17,3 +28,5 @@ const Form = () => {
         </form>
     );
 }
+
+export default Form
